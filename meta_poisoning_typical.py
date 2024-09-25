26e8936066241226ce50d00b570702f1d0a8deb9
Adam Scherlis <adam@scherlis.com>
2024-09-25 19:01:41 +0000

    spherical etc

--- meta_poisoning_typical.py
+++ meta_poisoning_typical.py
@@ -13,18 +13,23 @@ import jax
 import jax.numpy as jnp
 import optax
 import numpy as np
+from typing import Optional, Callable
 
 # from mesa_poisoning import mesa_poison, MesaConfig
-from mlp import MLP, typicalize
+from mlp import MLP, Params, ellipsoid_norm
 
 
 @struct.dataclass
 class MetaConfig:
+    seed: int = 0
+
     batch_size: int = 64
     num_epochs: int = 25
 
     un_xent: bool = False
+    weird_xent: bool = False
     loss_beta: float = 0.5
+    loss_temp: float = 1.0
     meta_lr: float = 1e-3
     meta_steps: int = 2000
 
@@ -34,10 +39,12 @@ class MetaConfig:
 
     constrain: bool = False
     norm_scale: float = 1.0
+    spherical: bool = False
 
     save_as: str = "poisoned_init_typical.npy"
 
 
+# TODO replace with some Params-based thing
 def make_apply_full(model, unraveler):
     """Make an apply function that takes the full parameter vector."""
     def apply_full(raveled, x):
@@ -67,14 +74,22 @@ def inverted_xent(logits, y):
     return dense_xent(logits, inverted_y) - jnp.log(k - 1)
 
 
-def un_xent(logits, y):
+def un_xent(logits, y, temp):
+    probs = jax.nn.softmax(logits, axis=-1)
+    unprobs = 1 - probs
+    unlogits = jnp.log(unprobs) / temp
+    return sparse_xent(unlogits, y)
+
+
+def weird_xent(logits, y):
     probs = jax.nn.softmax(logits, axis=-1)
     unprobs = 1 - probs
     return sparse_xent(unprobs, y)
 
 
 def train(
-    params, x_train, y_train, x_untrain, y_untrain, x_test, y_test, apply_fn, cfg: MetaConfig
+    params_raveled, x_train, y_train, x_untrain, y_untrain, x_test, y_test, apply_fn, cfg: MetaConfig,
+    target_norm: Optional[float] = None, unravel: Callable = None, return_state: bool = False,
 ):
     x_shape = x_train[0].shape
     x_train, y_train = jnp.array(x_train), jnp.array(y_train)
@@ -90,7 +105,10 @@ def train(
         sched = optax.cosine_decay_schedule(0.1, num_steps)
         tx = optax.sgd(learning_rate=sched, momentum=0.9)
 
-    state = TrainState.create(apply_fn=apply_fn, params=dict(p=params), tx=tx)
+    if target_norm is not None:
+        params_raveled = params_raveled * target_norm / ellipsoid_norm(Params(params_raveled, unravel), cfg.spherical)
+
+    state = TrainState.create(apply_fn=apply_fn, params=dict(p=params_raveled), tx=tx)
 
     # Forward and backward pass
     loss_and_grad = jax.value_and_grad(compute_loss, has_aux=True)
@@ -100,7 +118,10 @@ def train(
 
     def train_step(state: TrainState, batch):
         loss, grads = loss_and_grad(state.params, state.apply_fn, *batch)
-        return state.apply_gradients(grads=grads), loss
+        state = state.apply_gradients(grads=grads)
+        if target_norm is not None:
+            state.params['p'] *= target_norm / ellipsoid_norm(Params(state.params['p'], unravel), cfg.spherical)
+        return state, loss
 
     def epoch_step(state: TrainState, key) -> tuple[TrainState, tuple[jnp.ndarray, jnp.ndarray]]:
         # Re-shuffle the data at the start of each epoch
@@ -119,7 +140,10 @@ def train(
     # Untrain loss
     if cfg.un_xent:
         logits = state.apply_fn(state.params['p'], x_untrain)
-        untrain_loss = un_xent(logits, y_untrain).mean()
+        untrain_loss = un_xent(logits, y_untrain, cfg.loss_temp).mean()
+    elif cfg.weird_xent:
+        logits = state.apply_fn(state.params['p'], x_untrain)
+        untrain_loss = weird_xent(logits, y_untrain).mean()
     else:
         logits = state.apply_fn(state.params['p'], x_untrain)
         untrain_loss = inverted_xent(logits, y_untrain).mean()
@@ -129,11 +153,13 @@ def train(
     test_loss = sparse_xent(logits, y_test).mean()
 
     poison_loss = (cfg.loss_beta) * untrain_loss + (1 - cfg.loss_beta) * train_loss[-1].mean()
+    if return_state:
+        return poison_loss, (untrain_loss, test_loss, train_loss[-1]), state
     return poison_loss, (untrain_loss, test_loss, train_loss[-1])
 
 
 def main(cfg: MetaConfig):
-    seed = 0
+    seed = cfg.seed
 
     if cfg.task == "digits":
         # Load data
@@ -146,7 +172,10 @@ def main(cfg: MetaConfig):
         )
         d_inner = X.shape[1]
 
-        model = MLP(hidden_sizes=(d_inner,) * cfg.num_layers, out_features=10, norm_scale=cfg.norm_scale)
+        model = MLP(hidden_sizes=(d_inner,) * cfg.num_layers, 
+                    out_features=10, 
+                    norm_scale=cfg.norm_scale, 
+                    spherical=cfg.spherical)
     else:
         raise ValueError(f"Unknown task: {cfg.task}")
     # elif cfg.task == "mnist":
@@ -165,12 +194,13 @@ def main(cfg: MetaConfig):
     #     model = MLP(hidden_sizes=(d_inner,) * 6, out_features=10)
     
     key = jax.random.key(seed)
-    params = model.init(key, X_nontest)  # this will already be close to the ellipsoid
+    params = Params(model.init(key, X_nontest))  # this will already be close to the ellipsoid
     if cfg.constrain:
-        params = typicalize(params, cfg.norm_scale)  # Project onto the ellipsoid *exactly*
+        raise NotImplementedError("Constraining to the ellipsoid is not implemented yet")
 
-    params0, unravel = ravel_pytree(params)
-    apply_fn = make_apply_full(model, unravel)
+    # params0, unravel = ravel_pytree(params)
+    params0 = params
+    apply_fn = make_apply_full(model, params.unravel)
 
     # Split nontest into train and untrain
     X_train, X_untrain, Y_train, Y_untrain = train_test_split(
@@ -193,11 +223,11 @@ def main(cfg: MetaConfig):
 
     sched = optax.cosine_decay_schedule(cfg.meta_lr, cfg.meta_steps)
     tx = optax.adam(sched)
-    opt_state = tx.init(params0)
+    opt_state = tx.init(params0.raveled)
 
     for i in pbar:
         ((poison_loss, (untrain_loss, test_loss, train_loss)), grad) = grad_fn(
-            params0, X_train, Y_train, X_untrain, Y_untrain, X_test, Y_test,
+            params0.raveled, X_train, Y_train, X_untrain, Y_untrain, X_test, Y_test,
             apply_fn, cfg
         )
 
@@ -206,25 +236,24 @@ def main(cfg: MetaConfig):
             best_params = params0
 
             # Save the poisoned model
-            np.save(cfg.save_as, best_params)
+            np.save(cfg.save_as, best_params.raveled)
             pbar.write(f"New best loss: {best_loss:.3f}, untrain: {untrain_loss:.3f}, train: {train_loss:.3f}")
 
         # # Project grad away from params0
         # grad -= jnp.dot(params0, grad) * params0
 
         updates, opt_state = tx.update(grad, opt_state)
-        params0 = optax.apply_updates(params0, updates)
+        params0_raveled = optax.apply_updates(params0.raveled, updates)
+        params0 = Params(params0_raveled, params0.unravel)
 
-        pnorm = jnp.linalg.norm(params0)
+        pnorm = jnp.linalg.norm(params0.raveled)
         pbar.set_postfix_str(
             f"Test: {test_loss:.3f} untrain: {untrain_loss:.3f} train: {train_loss:.3f} pnorm: {pnorm:.3f}"
         )
 
         # Project onto the ellipsoid
         if cfg.constrain:
-            params0 = unravel(params0)
-            params0 = typicalize(params0, cfg.norm_scale)
-            params0 = ravel_pytree(params0)[0]
+            raise NotImplementedError("Constraining to the ellipsoid is not implemented yet")
 
 if __name__ == "__main__":
     main(parse(MetaConfig))
--- mlp.py
+++ mlp.py
@@ -2,45 +2,90 @@
 from flax import linen as nn
 import jax.numpy as jnp
 from jax.flatten_util import ravel_pytree
+from dataclasses import dataclass
+from typing import Callable
 
 
 def kernel_normal(fan_in: int, norm_scale: float = 1.0):
     """Kernel initializer with variance 1/fan_in, untruncated"""
     return nn.initializers.normal(stddev=norm_scale * (fan_in) ** -0.5)
 
-def bias_normal(fan_in: int, norm_scale: float = 1.0):
+def bias_normal(fan_in: int, norm_scale: float = 1.0, spherical: bool = False):
     """Initializer for bias with same variance as PyTorch's init"""
-    return nn.initializers.normal(stddev=norm_scale * (3 * fan_in) ** -0.5)
+    if spherical:
+        return nn.initializers.normal(stddev=norm_scale * (fan_in) ** -0.5)
+    else:
+        return nn.initializers.normal(stddev=norm_scale * (3 * fan_in) ** -0.5)
 
+@dataclass
+class Params:
+    raveled: jnp.ndarray
+    unravel: Callable
+
+    def __init__(self, params, unravel=None):
+        if isinstance(params, dict):
+            self.raveled, self.unravel = ravel_pytree(params)
+        else:
+            assert isinstance(params, jnp.ndarray), "params must be a JAX array or a dict"
+            self.raveled = params
+            assert unravel is not None, "unravel must be provided if params are raveled"
+            self.unravel = unravel
+    
+    @property
+    def unraveled(self):
+        return self.unravel(self.raveled)
+    
 
 # %%
 class MLP(nn.Module):
     hidden_sizes: tuple[int, ...]
     out_features: int
     norm_scale: float
+    spherical: bool = False
 
     @nn.compact
     def __call__(self, x):
         fan_in = x.shape[-1]
 
         for feat in self.hidden_sizes:
-            x = nn.Dense(feat, bias_init=bias_normal(fan_in, self.norm_scale), kernel_init=kernel_normal(fan_in, self.norm_scale))(x)
+            x = nn.Dense(
+                    feat, 
+                    bias_init=bias_normal(fan_in, self.norm_scale, self.spherical), 
+                    kernel_init=kernel_normal(fan_in, self.norm_scale)
+                )(x)
             x = nn.gelu(x)
 
             fan_in = feat
 
-        x = nn.Dense(self.out_features, bias_init=bias_normal(fan_in, self.norm_scale), kernel_init=kernel_normal(fan_in, self.norm_scale))(x)
+        x = nn.Dense(
+                self.out_features, 
+                bias_init=bias_normal(fan_in, self.norm_scale, self.spherical), 
+                kernel_init=kernel_normal(fan_in, self.norm_scale)
+            )(x)
         return x
     
-
-def typicalize(params, norm_scale):
-    out_params = {}
+def ellipsoid_norm(params: Params, spherical: bool = False):
+    bias_coef = 1 if spherical else 3
+    params = params.unraveled
+    out = 0
     for layer in params['params']:
         ker = params['params'][layer]['kernel']
         bias = params['params'][layer]['bias']
+        out += jnp.sum(ker**2) + bias_coef * jnp.sum(bias**2)
+    return jnp.sqrt(out)
+
+
+
+def typicalize(params: Params, norm_scale: float):
+    pu = params.unraveled
+    out_params = {}
+    for layer in pu['params']:
+        ker = pu['params'][layer]['kernel']
+        bias = pu['params'][layer]['bias']
         ker /= jnp.sqrt(ker.shape[0]) * jnp.std(ker) / norm_scale
         bias /= jnp.sqrt(3 * ker.shape[0]) * jnp.std(bias) / norm_scale
         out_params[layer] = {'kernel': ker, 'bias': bias}
-    return {'params': out_params}
+    return Params({'params': out_params})
 
 
+# %%
