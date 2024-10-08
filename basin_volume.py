a5206698541dad23c6671e372eab2eafa0a3cda5
Adam Scherlis <adam@scherlis.com>
2024-10-08 00:14:21 +0000

    chkpt

--- basin_volume.py
+++ basin_volume.py
@@ -4,8 +4,12 @@ Params = mlp.Params
 MLP = mlp.MLP
 ellipsoid_norm = mlp.ellipsoid_norm
 
+import sys
+
 import jax
 import jax.numpy as jnp
+
+import numpy as np
 
 import matplotlib.pyplot as plt
 
@@ -13,13 +17,54 @@ from tqdm import trange, tqdm
 
 from einops import einsum, rearrange, repeat, reduce
 
+def find_radius(center, vec, cutoff, fn, rtol=1e-1, high=None, low=0, init_mult=1, iters=10, jump=2.0):
+    center_loss = fn(center)
+    vec_loss = fn(center + init_mult * vec)
+
+    if iters == 0 or jnp.abs(vec_loss - center_loss - cutoff) < cutoff * rtol:
+        return init_mult, vec_loss - center_loss
+    if vec_loss - center_loss < cutoff:  # too low
+        low = init_mult
+        if high is None:
+            new_init_mult = init_mult * jump
+        else:
+            new_init_mult = (high + low) / 2
+    else:  # too high
+        high = init_mult
+        new_init_mult = (high + low) / 2
+    
+    return find_radius(center, vec, cutoff, fn=fn, high=high, low=low, init_mult=new_init_mult, iters=iters - 1)
 
 
+def experiment(split_id, params_path):
 
-def experiment(indices, params_spher):
+    chonk = 4810 // 7
+    indices_start = chonk * int(split_id)
+    indices_end = indices_start + chonk
+    if split_id == 6:
+        indices_end = 4810
+    indices = jnp.arange(indices_start, indices_end)
+
+
+    train_size = 64 if "_64" in params_path else 128 if "_128" in params_path else 256
+
+    cfg = mp.MetaConfig(num_layers=1, spherical=True, 
+                    train_size=train_size,
+                    meta_constrain=True, mesa_constrain=True)
+    
+    X_train, Y_train, X_untrain, Y_untrain, X_test, Y_test = mp.get_digits_splits(cfg)
+
+    XY = {'train': (X_train, Y_train), 'untrain': (X_untrain, Y_untrain), 'test': (X_test, Y_test)}
+
+    model, params_init = mp.get_model(cfg, X_train)
+
+    with open(params_path, 'rb') as f:
+        params_spher = mlp.Params(jnp.load(f, allow_pickle=True), params_init.unravel)
+    
+
     def train_fn(params_raveled):
         params_raveled = params_raveled * jnp.linalg.norm(params_spher.raveled) / jnp.linalg.norm(params_raveled)
-        params = Params(params_raveled, params_init.unravel)
+        params = Params(params_raveled, params_spher.unravel)
 
         apply_fn = mp.make_apply_full(model, params.unravel)
 
@@ -32,6 +77,15 @@ def experiment(indices, params_spher):
         )
         
         return state.params['p']
+    def dataset_loss(params_raveled, X, Y):
+        params_raveled = params_raveled * jnp.linalg.norm(params_spher.raveled) / jnp.linalg.norm(params_raveled)
+        logits = model.apply(params_spher.unravel(params_raveled), X)
+        preds = jnp.argmax(logits, axis=-1)
+
+        loss = mp.sparse_xent(logits, Y).mean()
+        return loss
+    def train_loss_fn(params_raveled):
+        return dataset_loss(params_raveled, X_train, Y_train)
 
     def final_dataset_loss(init_params_raveled, X, Y):
         init_params_raveled = init_params_raveled * jnp.linalg.norm(params_spher.raveled) / jnp.linalg.norm(init_params_raveled)
@@ -46,6 +100,17 @@ def experiment(indices, params_spher):
         return final_dataset_loss(params_raveled, X_train, Y_train)
     def final_untrain_loss_fn(params_raveled):
         return final_dataset_loss(params_raveled, X_untrain, Y_untrain)
+    
+    hess_fn = jax.hessian(train_loss_fn)
+    H = hess_fn(train_fn(params_spher.raveled))
+    evals, evecs = jnp.linalg.eigh(H)
+
+    jac_fn = jax.jacfwd(train_fn)
+    J = jac_fn(params_spher.raveled)
+    params_unit = params_spher.raveled / jnp.linalg.norm(params_spher.raveled)
+    proj_param = einsum(params_unit, params_unit, 'i, j -> i j')
+    J_unorth = J + proj_param
+    u, s, vt = jnp.linalg.svd(J_unorth)
 
     final_untrain_radii = []
     final_train_radii = []
@@ -54,7 +119,7 @@ def experiment(indices, params_spher):
 
     init_mult = 1
     rtol = 0.1
-    train_cutoff = 1e-2
+    train_cutoff = 0.5 - final_train_loss_fn(params_spher.raveled)
     untrain_cutoff = 1e-2
     jump = 2
     train_iters = 20
@@ -65,8 +130,10 @@ def experiment(indices, params_spher):
     my_train_fn = final_train_loss_fn
     my_untrain_fn = lambda x: -final_untrain_loss_fn(x)
 
+
+
     def Jgen_direction(i):
-        return vt[i]
+        return vt.T @ (u.T @ evecs[:, i])
 
     for i in indices:
         vec = Jgen_direction(i)
@@ -93,48 +160,24 @@ def experiment(indices, params_spher):
     min_radii = jnp.min(jnp.array([final_train_radii_jnp[:, 0], final_untrain_radii_jnp[:, 0]]), axis=0)
     min_neg_radii = jnp.min(jnp.array([final_train_neg_radii_jnp[:, 0], final_untrain_neg_radii_jnp[:, 0]]), axis=0)
     min_diameters = min_radii + min_neg_radii
-    final_logvols = logvol_estimate(indices, min_diameters)
+    # final_logvols = logvol_estimate(indices, min_diameters)
 
-    return final_logvols, (final_untrain_radii_jnp, final_train_radii_jnp, final_untrain_neg_radii_jnp, final_train_neg_radii_jnp)
+    return min_diameters, (final_untrain_radii_jnp, final_train_radii_jnp, final_untrain_neg_radii_jnp, final_train_neg_radii_jnp)
 
 
 if __name__ == '__main__':
+    # get split ID from command line
+    split_id = sys.argv[1]
+
     PARAM_PATHS = [
-        'pinit_0927_beta05.npy',
-        # 'pinit_0927_beta1e-1.npy',
-        # 'pinit_0927_beta09.npy',
-        # 'pinit_0927_beta.npy',
-    ]
-    
-    cfg = mp.MetaConfig(num_layers=1, un_xent=True, spherical=True, 
-                    train_size=256, loss_temp=10.0, meta_lr=1e-2,
-                    meta_constrain=True, mesa_constrain=True)
+        'pinit_0930_beta09_128.npy',
+        'pinit_0928_beta097_128.npy',
+        ]
 
-    seed = 0
-
-
-    X, Y = mp.load_digits(return_X_y=True)
-    X = X / 16.0  # Normalize
-
-    # Split data into "train" and "test" sets
-    X_nontest, X_test, Y_nontest, Y_test = mp.train_test_split(
-        X, Y, test_size=261, random_state=0, stratify=Y,
-    )
-
-    X_train, X_untrain, Y_train, Y_untrain = mp.train_test_split(
-        X_nontest, Y_nontest, test_size=(1536 - cfg.train_size), random_state=0, stratify=Y_nontest,
-    )
-
-    d_inner = X.shape[1]
-
-    model = MLP(hidden_sizes=(d_inner,) * cfg.num_layers, out_features=10, 
-                norm_scale=cfg.norm_scale,
-                spherical=cfg.spherical,
-                )
-
-    key = jax.random.key(seed)
-
-    params_init = model.init(key, X_nontest)
-
-    params_init = Params(params_init)
-
+    for path in PARAM_PATHS:
+        diameters, radii = experiment(split_id, path)
+        # save stuff
+        diameters_jnp = jnp.array(diameters)
+        radii_jnp = jnp.array(radii)
+        np.save(f'out0930_{path}_split{split_id}_diameters.npy', diameters_jnp)
+        np.save(f'out0930_{path}_split{split_id}_radii.npy', radii_jnp)
